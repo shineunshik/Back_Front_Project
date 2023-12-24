92881735cf108e47b65eed5a42dd563f3310d2ff
shineunshik <dmstlrgkxm@naver.com>
2023-12-24 14:52:20 +0900

    info add

--- setting.sh
+++ setting.sh
@@ -3,11 +3,11 @@
 #mysqld &
 service nginx restart
 
-
+#spring project start
 cd /home/project/BE/build/libs
 chmod +x springboot-back-project-1.0-SNAPSHOT.jar
 nohup java -jar springboot-back-project-1.0-SNAPSHOT.jar > output.log 2>&1 &
 
-#service mysql start && tail -f /dev/null
+#mysql start && background start
 mysqld &
 tail -f /dev/null
